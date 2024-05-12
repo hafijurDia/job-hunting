6833e35e906da46ac08e7321f18fdc0b813f4747
hafijure <hafijure@gmail.com>
2024-05-12 17:46:59 +0600

    Reachart added

--- src/components/AppliedJob/Applied-Job.jsx
+++ src/components/AppliedJob/Applied-Job.jsx
@@ -7,54 +7,63 @@ import { useLoaderData, Link } from 'react-router-dom'
 export default function AppliedJob() {
   const savedCart = useLoaderData();
   const [jobs, setJobs] = useState(savedCart);
+  const [filter, setFilter] = useState('');
+
+  const handleSelectChange = (event) => {
+    const filterItem = event.target.value;
+    setFilter(filterItem);
+  };
+  const filteredJobs = filter ? jobs.filter(job => job.remote_or_onsite === filter) : jobs;
+
 
   return (
     <>
-      <Banner></Banner>
+      <Banner />
       <section className='pt-20'>
-        <div className="container mx-auto">
-
-          {
-            jobs.map(job=>
-              <div key={job.id} className="md:flex justify-center items-center gap-5 border rounded-lg p-10 mb-10">
-              <div className="basis-2/12">
-                <div className='h-full'>
-                <img src={job.logo} alt="" />
-                </div>
-                  
-              </div>
-              <div className="basis-10/12 "> 
-                  <div className="md:flex justify-between items-center">
-                  <div>
-                  <h5 className="text-2xl font-bold mb-2 text-[#474747]">
-            {job.job_title}
-          </h5>
-          <p className="text-xl font-semibold text-[#757575]">{job.company_name}</p>
-          <ul className="list-none my-6">
-            <li className="text-base border border-blue-300 p-2 text-center rounded w-2/12 font-bold active inline mr-5">
-              {job.remote_or_onsite}
-            </li>
-
-          </ul>
-          <p className="text-xl font-semibold text-[#757575]">
-            <span>Icon: {job.contact_information.address}</span>{" "}
-            <span>Icon: Salary: {job.salary}</span>
-          </p>
-                  </div>
-                  <div>
-                  <Link to={`jobdetails/${job.id}`}><button className="bg-gradient-to-r bg-[#7E90FE] bg-[#7450d5] text-white px-4 p-4 rounded-md hover:bg-blue-600 text-xl font-bold">
-            View Details
-          </button></Link>
-                  </div>
-                  </div>
-              </div>
-          </div>
-            )
-          }
-            
+        <div className="container mx-auto flex justify-end mb-10">
+          <select onChange={handleSelectChange} id="jobs" className="border border-gray-300 text-gray-900 rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-32 text-lg bg-slate-100">
+            <option value="">Filter</option>
+            <option value="Remote">Remote</option>
+            <option value="Onsite">Onsite</option>
+          </select>
         </div>
-    </section>
-    <Footer></Footer>
+        <div className="container mx-auto">
+          {filteredJobs.length > 0 ? (
+            filteredJobs.map(job => (
+              <div key={job.id} className="md:flex justify-center items-center gap-5 border rounded-lg p-10 mb-10">
+                <div className="basis-2/12">
+                  <div className='h-full'>
+                    <img src={job.logo} alt="" />
+                  </div>
+                </div>
+                <div className="basis-10/12 "> 
+                  <div className="md:flex justify-between items-center">
+                    <div>
+                      <h5 className="text-2xl font-bold mb-2 text-[#474747]">{job.job_title}</h5>
+                      <p className="text-xl font-semibold text-[#757575]">{job.company_name}</p>
+                      <ul className="list-none my-6">
+                        <li className="text-base border border-blue-300 p-2 text-center rounded w-2/12 font-bold active inline mr-5">{job.remote_or_onsite}</li>
+                      </ul>
+                      <p className="text-xl font-semibold text-[#757575]">
+                        <span>Icon: {job.contact_information.address}</span>{" "}
+                        <span>Icon: Salary: {job.salary}</span>
+                      </p>
+                    </div>
+                    <div>
+                      <Link to={`jobdetails/${job.id}`}>
+                        <button className="bg-gradient-to-r bg-[#7E90FE] bg-[#7450d5] text-white px-4 p-4 rounded-md hover:bg-blue-600 text-xl font-bold">View Details</button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="text-center text-gray-600 mt-5">No jobs match the selected filter.</div>
+          )}
+        </div>
+      </section>
+      <Footer />
     </>
-  )
+  );
 }
--- src/components/Statistics/Statistic.jsx
+++ src/components/Statistics/Statistic.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import React from 'react';
+import Banner from '../Banner/Banner';
+import { 
+  LineChart, 
+  Line, 
+  XAxis, 
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  Rectangle,
+  ComposedChart,
+  Area,
+  Scatter,
+  PieChart,
+  Pie,
+
+} from 'recharts';
+
+
+
+
+let marks= [
+  { id: 1, assignmentName: "Assignment 1", mark: 85 },
+  { id: 2, assignmentName: "Assignment 2", mark: 45 },
+  { id: 3, assignmentName: "Assignment 3", mark: 50 },
+  { id: 4, assignmentName: "Assignment 4", mark: 63 },
+  { id: 5, assignmentName: "Assignment 5", mark: 79 },
+  { id: 6, assignmentName: "Assignment 6", mark: 90 },
+  { id: 7, assignmentName: "Assignment 7", mark: 70 },
+  { id: 8, assignmentName: "Assignment 8", mark: 68 },
+ 
+]
 
 export default function Statistic() {
   return (
-    <div>
-      <h2>Statistice</h2>
-    </div>
+    <>
+      <Banner></Banner>
+      <div className='container mx-auto'>
+        <div className='chartarea w-100 text-center mt-20'>
+        <LineChart
+          width={1000}
+          height={300}
+          data={marks}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="assignmentName" />
+          <YAxis dataKey="mark" />
+          <Tooltip />
+          <Legend />
+          <Line dataKey="mark" stroke="#111111"></Line>
+  
+        </LineChart>
+
+        <BarChart
+          width={500}
+          height={300}
+          data={marks}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="assignmentName" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="mark" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+         
+        </BarChart>
+        <ComposedChart
+          width={500}
+          height={400}
+          data={marks}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="assignmentName" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+          <Bar dataKey="mark" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="mark" stroke="#ff7300" />
+          <Scatter dataKey="mark" fill="red" />
+        </ComposedChart>
+
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="assignmentName"
+            startAngle={180}
+            endAngle={0}
+            data={marks}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+        </PieChart>
+          </div> 
+      </div>
+        
+
+    </>
   )
 }
