46dc63d19a0085d2db6a4b4f95854842fbe331d6
hafijure <hafijure@gmail.com>
2024-05-12 13:33:26 +0600

    show more button working done

--- src/components/Home/FeaturedJob/FeaturedJob.jsx
+++ src/components/Home/FeaturedJob/FeaturedJob.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import google from "../../../../public/images/company-icon/google-1-1 1.png";
 import { Link } from "react-router-dom";
 import JobDetails from "../../JobDetails/JobDetails";
 
 const FeaturedJob = (Props) => {
   const jobs = Props.jobs;
-
+  const [showAllJobs, setShowAllJobs] = useState(false);
+  const jobsToShow = showAllJobs ? jobs : jobs.slice(0,4);
   
+  const handleShowMore = () => {
+    setShowAllJobs(true);
+  };
+
   return (
     <>
     <div className="container mx-auto md:grid grid-cols-2 gap-5">
+
       {
-        jobs.map(job => 
+        jobsToShow.map(job => 
           <div key={job.id} className="basis-2/4 rounded-lg p-10 border">
       <div className="icon">
         <img src={job.logo} alt="" />
@@ -23,11 +29,9 @@ const FeaturedJob = (Props) => {
         <p className="text-xl font-semibold text-[#757575]">{job.company_name}</p>
         <ul className="list-none my-6">
           <li className="text-base border border-blue-300 p-2 text-center rounded w-2/12 font-bold active inline mr-5">
-            Remote
+            {job.remote_or_onsite}
           </li>
-          <li className="text-base border border-blue-300 p-2 text-center rounded w-2/12 font-bold active inline mr-5">
-            Full Time
-          </li>
+
         </ul>
         <p className="text-xl font-semibold text-[#757575]">
           <span>Icon: {job.location} </span> 
@@ -44,7 +48,7 @@ const FeaturedJob = (Props) => {
   </div>
   <div className="container mx-auto">
     <div className="w-100 text-center py-10">
-    <button className="bg-gradient-to-r bg-[#7E90FE] bg-[#7450d5] text-white px-4 p-4 rounded-md hover:bg-blue-600 text-xl font-bold">
+    <button onClick={() => handleShowMore()} className="bg-gradient-to-r bg-[#7E90FE] bg-[#7450d5] text-white px-4 p-4 rounded-md hover:bg-blue-600 text-xl font-bold">
           See All Jobs
         </button>
     </div>
